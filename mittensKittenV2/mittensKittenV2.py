070f14c0ece702ab83a9739e0aa617f1c853b300
Miranda Henning <35973769+mhenning3@users.noreply.github.com>
2023-07-10 19:33:49 -0400

    Added pig latin feature
    .

--- mittensKittenV2/mittensKittenV2.py
+++ mittensKittenV2/mittensKittenV2.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 import nextcord
-
+import string
 
 intents = nextcord.Intents.default().all()
 
@@ -31,22 +31,67 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     print(f'Message from {message.author}: {message.content}')
-    if message.content.startswith('nani'):
+    if message.content.startswith('nani'.lower()):
         channel = message.channel
         await channel.send('UwU')
+    if message.content.startswith('test'.lower()):
+        channel = message.channel
+        await channel.send('ed')
 
 
-
-@bot.slash_command(description="ping", guild_ids=[GuildId])
+#only use lowercase for names
+@bot.slash_command(description="ping" )
 async def ping(interaction: nextcord.Interaction):
     await interaction.send("pong")
 
-@bot.slash_command(description="dice roll", guild_ids=[GuildId])
-async def ping(interaction: nextcord.Interaction):
-    await interaction.send("dice")
+@bot.slash_command(name="dice_roll",description="dice_roll", guild_ids=[GuildId])
+async def dice_roll(interaction: nextcord.Interaction):
+    await interaction.send("I don't work yet. Sorry")
+
+
+vowels = ["a","e","i","o","u","y"]
+def vowelChecker(arg):
+
+    for y in range(len(vowels)):
+        if arg[0].lower()==vowels[y]:
+            return True
+
+def VowelModifier(arg):
+    return (arg+"yay").lower()
+
+def consonantModifier(arg):
+    count=0
+    for y in range(len(arg)):
+        if not vowelChecker(arg[y]):
+            count=count+1
+        else:
+            break
+    if(arg[1].lower()=="y"):
+        count=2
+    return (arg[count:len(arg)]+arg[0:count]+"ay").lower()
 
 
 
+@bot.slash_command(name="pig_latin",description="pig_latin")
+async def pig_latin(interaction: nextcord.Interaction, word: str):
+    #await interaction.response.send_message(f"You said: {arg[0:len(arg)-1]}")
+    arg=word.split()
+    finalWord=""
+    for x in range(len(arg)):
+        arg[x]=arg[x].strip(string.punctuation)
+
+        if(len(arg[x])>1):
+            if vowelChecker(arg[x]):
+                finalWord=finalWord+VowelModifier(arg[x])+" "
+                
+            else:
+                #consonantModifier(arg)
+                finalWord=finalWord+consonantModifier(arg[x])+" "
+        elif(len(arg[x])==1):
+            finalWord=finalWord+(arg[x].upper())+" "
+        else:
+            return
+    await interaction.response.send_message(f"Original sentence:\t{word}\nPig latin:\t{finalWord}")
 
  
 bot.run(BotToken)
